4315d44a41a27e2b7a77ec5c40404c3f2e56b29a
HyeonJeong Yong <47441135+charotiti9@users.noreply.github.com>
2019-05-08 14:29:19 +0900

    Find gameObjects with layer

--- SearchLayer.cs
+++ SearchLayer.cs
@@ -1,22 +1,21 @@
-    /// <summary>
-    /// 레이어로 오브젝트들을 탐색한다.
-    /// (현재는 UI 레이어 검색 시 사용중)
-    /// </summary>
-    /// <param name="layer">검색할 레이어</param>
-    /// <returns></returns>
-    GameObject[] FindGameObjectsWithLayer(int layer)
+/// <summary>
+/// 레이어로 오브젝트들을 탐색한다.
+/// (현재는 UI 레이어 검색 시 사용중)
+/// </summary>
+/// <param name="layer">검색할 레이어</param>
+/// <returns></returns>
+GameObject[] FindGameObjectsWithLayer(int layer)
+{
+    GameObject[] goArray = FindObjectsOfType(typeof(GameObject)) as GameObject[];
+    List<GameObject> goList = new List<GameObject>();
+    for (var i = 0; i< goArray.Length; i++) 
     {
-        GameObject[] goArray = FindObjectsOfType(typeof(GameObject)) as GameObject[];
-        List<GameObject> goList = new List<GameObject>();
-        for (var i = 0; i< goArray.Length; i++) 
+        if (goArray[i].layer == layer) 
         {
-            if (goArray[i].layer == layer) 
-            {
-                goList.Add(goArray[i]);
-            }
+            goList.Add(goArray[i]);
         }
-        if (goList.Count == 0) 
-            return null;
-        return goList.ToArray();
     }
+    if (goList.Count == 0) 
+        return null;
+    return goList.ToArray();
 }
